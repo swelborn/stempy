03e0468ef5c0e561fece7b78bc83960ec6a7b67e
swelborn <swelborn@lbl.gov>
2023-01-09 02:17:54 -0500

    trigger-ci

--- build_docker_swelborn.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-docker buildx build --platform linux/amd64 --load --tag samwelborn/stempy-swelborn:latest-amd64 -f ./docker/Dockerfile.swelborn-full-amd ./docker
-
-docker buildx build --platform linux/arm64 --load --tag samwelborn/stempy-swelborn:latest-arm64 -f ./docker/Dockerfile.swelborn-full-arm ./docker
-
-docker push samwelborn/stempy-swelborn:latest-arm64
-
-docker push samwelborn/stempy-swelborn:latest-amd64
