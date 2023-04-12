bb970fa88f6c621bc069cd1436175088d895c606
Sam Welborn <swelborn@lbl.gov>
2023-04-12 04:10:35 +0000

    change to release version

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-cmake -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -B build  -Dstempy_ENABLE_VTKm:BOOL=OFF -Dstempy_ENABLE_MPI:BOOL=ON -Dstempy_ENABLE_ZMQ:BOOl=ON
+cmake -DCMAKE_BUILD_TYPE:STRING=Release -B build  -Dstempy_ENABLE_VTKm:BOOL=OFF -Dstempy_ENABLE_MPI:BOOL=ON -Dstempy_ENABLE_ZMQ:BOOl=ON
 cmake --build build -j16
